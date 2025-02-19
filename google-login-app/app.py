43162f5bb36cf7c9b3ce3b93391b3a5e096f608f
sammisam8888 <samuelpriyatam@gmail.com>
2025-02-19 12:53:55 +0530

    updated-secret-import

--- google-login-app/app.py
+++ google-login-app/app.py
@@ -5,7 +5,7 @@ from google.oauth2 import id_token
 from pip._vendor import cachecontrol
 from google_auth_oauthlib.flow import Flow
 import google.auth.transport.requests 
-from .secret import password,clientid
+from secret import password,clientid
 
 app=Flask("Google Login App")
 app.secret_key=password
